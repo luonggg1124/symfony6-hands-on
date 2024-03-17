4c3d18d34b3a7950cc35982d9a286ec12bd1ee28
luonggg1124 <luongnm1124@gmail.com>
2024-03-17 22:00:59 +0700

    17/3/2024

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,19 @@
+<?php 
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HelloController
+{
+    private array $messages = [
+        "Hello"
+    ];
+    #[Route('/', name:'app_index')]
+    public function index():Response
+    {
+        return new Response('Hi');
+    }
+
+    
+}
