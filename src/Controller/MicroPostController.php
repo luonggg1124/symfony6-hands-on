d7512feccc901030ee9fcfb024ce283ef0a25967
luonggg1124 <luongnm1124@gmail.com>
2024-03-25 20:27:55 +0700

    25/3/2024

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\MicroPost;
 use App\Repository\MicroPostRepository;
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,9 +14,36 @@ class MicroPostController extends AbstractController
     #[Route('/micro/post', name: 'app_micro_post')]
     public function index(MicroPostRepository $posts): Response
     {
-        // dd($posts->findAll());
+        // $microPost = new MicroPost();
+        // $microPost->setTitle('It comes from controller');
+        // $microPost->setText('Hi!');
+        // $microPost->setCreated(new DateTime());
+        // $microPost = $posts->find(4);
+        // $microPost->setTitle('Welcome in general');
+
+
+        // $posts->remove($microPost,true);
+
+        // dd($posts->findBy(['title' => 'Welcome to VietNam!']));
+        dd($posts->findAll());
         return $this->render('micro_post/index.html.twig', [
-            'controller_name' => 'MicroPostController',
+            'posts' => $posts->findAll(),
         ]);
+    }
+    #[Route('/micro-post/{post}',name:'app_micro_post_show')]
+    public function showOne(MicroPost $post): Response
+    {
+        // dd($post);
+        return $this->render('micro_post/show.html.twig', [
+            'post' => $post,
+        ]);
+    }
+    #[Route('/micro-post',name:'app_micro_post_all')]
+    public function showAll(MicroPostRepository $post): Response
+    {
+        return $this->render('micro_post/index.html.twig', [
+            'posts' => $post->findAll(),
+        ]);
+       
     }
 }
