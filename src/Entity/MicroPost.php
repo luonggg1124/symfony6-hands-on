badfd32b786c7a6a45c19e584db388fe65f98678
luonggg1124 <luongnm1124@gmail.com>
2024-03-27 22:20:34 +0700

    27/3/2024

--- src/Entity/MicroPost.php
+++ src/Entity/MicroPost.php
@@ -5,6 +5,8 @@ namespace App\Entity;
 use App\Repository\MicroPostRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
 
 #[ORM\Entity(repositoryClass: MicroPostRepository::class)]
 class MicroPost
@@ -15,9 +17,13 @@ class MicroPost
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank()]
+    #[Assert\Length(min:5, max:255, minMessage:'Title is too short, 5 characters is the minium.')]
     private ?string $title = null;
 
     #[ORM\Column(length: 500)]
+    #[Assert\NotBlank()]
+    #[Assert\Length(min:5, max:500, minMessage:'Title is too short, 5 characters is the minium.')]
     private ?string $text = null;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
